4bf03f53c742d31ed4847cf38cf0bf9dcdea6bb3
khalid <khalidmiya399@gmail.com>
2025-09-18 06:48:46 +0000

    added dockerfile

---
+++ Dockerfile
@@ -0,0 +1,21 @@
+# Simple single-stage Dockerfile
+FROM node:18
+WORKDIR /app
+
+# Copy package files
+COPY package*.json ./
+
+# Install all dependencies
+RUN npm install
+
+# Copy all source code
+COPY . .
+
+# Generate Prisma client
+RUN npx prisma generate
+
+# Build the app
+RUN npm run build
+
+# Start the app
+CMD ["npm", "run", "start"]
--- src/routes/booking.route.ts
+++ src/routes/booking.route.ts
@@ -1,10 +1,10 @@
 import express from "express";
-import { bookSeat, getAllSeats } from "../controllers/booking.controller.js";
+import { bookSeat, cancelBooking, getAllSeats } from "../controllers/booking.controller.js";
 
 const router = express.Router();
 
 router.post("/shows/:showId/reserve",bookSeat);
 router.get("/shows/:showId/seats",getAllSeats);
-router.patch("/reservations/:reservationId/cancel");
+router.patch("/reservations/:reservationId/cancel",cancelBooking);
 
 export default router;
